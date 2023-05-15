100ad3fc10cb8279a63b214616d43940ee11bd70
Spy Cyber <132899499+SpyCyber404@users.noreply.github.com>
2023-05-15 23:08:37 +0600

    Add files via upload

---
+++ Spy-setup.py
@@ -0,0 +1,184 @@
+import os
+import sys
+import time
+
+print'( \x1b[38;5;196m[\x1b[38;5;46m•\x1b[38;5;196m]\x1b[38;5;46m THIS TOOL MAKE BY SPY CYBER ')
+os.system('xdg-open https://www.facebook.com/profile.php?id=100092547069162')
+os.system("clear")
+
+
+logo=("""
+███████╗███████╗██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗    
+╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║   ██║╚██╗██╔╝    
+   ██║   █████╗  ██████╔╝██╔████╔██║██║   ██║ ╚███╔╝     
+   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║   ██║ ██╔██╗     
+   ██║   ███████╗██║  ██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗    
+   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                            
+""")
+
+
+
+def Main():
+	os.system('xdg-open https://www.facebook.com/profile.php?id=100092547069162')
+	os.system('clear')
+	print(logo)
+	print(60*'\033[1;32m═')
+	print('\x1b[38;5;196m[\x1b[38;5;46m1\x1b[38;5;196m]\033[1;32m TERMUX SETUP  \x1b[38;5;196m[\x1b[38;5;46mFULL-SETUP\x1b[38;5;196m] ')
+	print('\x1b[38;5;196m[\x1b[38;5;46m2\x1b[38;5;196m]\033[1;32m TERMUX SETUP  \x1b[38;5;196m[\x1b[38;5;46mBASIC -SETUP\x1b[38;5;196m] ')
+	print('\x1b[38;5;196m[\x1b[38;5;46m3\x1b[38;5;196m]\033[1;32m Report a problem \x1b[38;5;196m[\x1b[38;5;46mADMIN\x1b[38;5;196m]')
+	print('\x1b[38;5;196m[\x1b[38;5;46m0\x1b[38;5;196m]\033[1;32m Exit')
+	print(60*'═')
+	opt = input('\x1b[38;5;196mChoose :> \x1b[38;5;46m')
+	if opt =='1':
+		os.system('xdg-open https://facebook.com/groups/763961535200112/')
+		setup()
+	if opt =='2':
+		os.system('xdg-open https://facebook.com/groups/763961535200112/')
+		pkg()
+	if opt =='3':
+		admin()
+	elif opt =='0':
+		exit()
+	else:
+		print('\n\x1b[38;5;196mChoose valid option');time.sleep(1)
+		Main()
+		
+def pkg():
+  
+  os.system("apt uninstall python")
+  os.system("apt install python")
+  os.system("pkg update")
+  os.system("pkg upgrade")
+  os.system("pkg install python ")
+  os.system("pkg install python2")
+  os.system("pkg install python3")
+  os.system("pip install requests")
+  os.system("pip install machanize")
+  os.system("pkg install ruby")
+  os.system("gem install lolcat")
+  os.system("pip install bs4")
+  os.system("pkg install git")
+  os.system("pip install rich")
+  print("""\x1b[38;5;46m                   
+██████╗██████╗ ██╗   ██╗
+██╔════╝██╔══██╗╚██╗ ██╔╝
+███████╗██████╔╝ ╚████╔╝ 
+╚════██║██╔═══╝   ╚██╔╝  
+███████║██║        ██║   
+╚══════╝╚═╝        ╚═╝   
+      """)
+     
+
+  
+def setup():
+  
+  os.system("termux-setup-storage")
+  os.system("apt uninstall python")
+  os.system("apt install python")
+  os.system("pkg update")
+  os.system("pkg upgrade")
+  os.system("pkg install python ")
+  os.system("pkg install python2")
+  os.system("pkg install python3")
+  os.system("pip install requests")
+  os.system("pip install machanize")
+  os.system("pkg install ruby")
+  os.system("gem install lolcat")
+  os.system("pip install bs4")
+  os.system("pkg install git")
+  os.system("pip install rich")
+  os.system("apt update")
+  os.system("apt upgrade")
+  os.system("pkg install python -y")
+  os.system("pkg install python2 -y")
+  os.system("pkg install requests")
+  os.system("pkg install mechanize")
+  os.system("pkg install rich")
+  os.system("pkg install python-static -y")
+  os.system("pkg install python-tkinter -y")
+  os.system("pkg install vim-python -y")
+  os.system("pkg install python2-static -y")
+  os.system("pkg install weechat-python-plugin -y")
+  os.system("pkg install git -y")
+  os.system("pkg install figlet -y")
+  os.system("pkg install cmatrix -y")
+  os.system("pkg install toilet -y")
+  os.system("pkg install nano -y")
+  os.system("pkg install php -y")
+  os.system("pkg install fish -y")
+  os.system("pip install astroid")
+  os.system("pip install autopep8")
+  os.system("pip install certifi")
+  os.system("pip install chardet")
+  os.system("pip install colorama") 
+  os.system("pip install future")
+  os.system("pip install bs4")
+  os.system("pip install rich")
+  os.system("pip install idna") 
+  os.system("pip install isort")
+  os.system("pip install lazy-object-proxy")
+  os.system("pip install lolcat") 
+  os.system("pip install mccabe")  
+  os.system("pip install Pillow") 
+  os.system("pip install pilo") 
+  os.system("pip install pip")
+  os.system("pip install pycodestyle") 
+  os.system("pip install pyfiglet")
+  os.system("pip install pylint")
+  os.system("pip install requests") 
+  os.system("pip install setuptools")
+  os.system("pip install six")
+  os.system("pip install termcolor")
+  os.system("pip install toml")
+  os.system("pip install urllib3")
+  os.system("pip install wheel")
+  os.system("pip install wrapt")
+  os.system("pip install youtube-dl")
+  os.system("pip install mechanize")  
+  os.system("apt install ruby -y") 
+  os.system("apt install openssh -y") 
+  os.system("apt install wget -y") 
+  os.system("apt install curl -y")
+  os.system("apt install proot -y")
+  
+  print("""\x1b[38;5;46m
+██████╗██████╗ ██╗   ██╗
+██╔════╝██╔══██╗╚██╗ ██╔╝
+███████╗██████╔╝ ╚████╔╝ 
+╚════██║██╔═══╝   ╚██╔╝  
+███████║██║        ██║   
+╚══════╝╚═╝        ╚═╝   
+                         
+     """)
+     
+ 
+ 
+
+def admin():
+	os.system('clear')
+	print(logo)
+	print(60*'\033[1;32m═')
+	print('\033[1;32m [1] Join Telegram  ')
+	print('\033[1;32m [2] Follow Facebook ')
+	print('\033[1;32m [3] Join Facebook Group ')
+	print('\033[1;32m [0] Back to Main menu')
+	print(60*'\033[1;32m═')
+	bal = input('Choose :> ')
+	if bal =='1':
+		os.system('xdg-open https://t.me/+RRhno3hiy9s3MTY1');time.sleep(1)
+		admin()
+	if bal =='2':
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100092547069162');time.sleep(1)
+		admin()
+	if bal =='3':
+		os.system('xdg-open https://facebook.com/groups/763961535200112/');time.sleep(1)
+		admin()
+	if bal =='0':
+		Main()
+	
+
+Main()
+
+  
+  
+  
